7c211900a0d9e5a06af284ea432dc6d36c3001fb
Vince <houghta4@uwm.edu>
2020-11-30 22:47:54 -0600

    maven build added

---
+++ src/main/java/Main.java
@@ -0,0 +1,15 @@
+import GameLaunch.MainFrame;
+import util.LogHandler;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Main {
+    private static final Logger logger = Logger.getLogger("errorLogger");
+
+    public static void main(String[] args){
+        Thread.setDefaultUncaughtExceptionHandler((t, e) -> logger.log(Level.SEVERE, "Uncaught exception", new Exception(e)));
+        LogHandler.setup();
+        new MainFrame();
+    }
+}
